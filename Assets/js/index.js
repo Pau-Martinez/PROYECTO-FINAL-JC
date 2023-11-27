fec46c6715dd549f5830f05f28204a2729502d74
Pau-Martinez <pmartinez.2005.1109@gmail.com>
2023-11-26 22:28:22 -0500

    Se inicia barra menu y seccion de reseñas

---
+++ Assets/js/index.js
@@ -0,0 +1,33 @@
+const header = document.getElementById('header')
+const menuOpen = document.getElementById('menu-open')
+const menuClose = document.getElementById('menu-close')
+const navMenu = document.getElementById('nav-menu')
+
+//menu responsive
+
+if (menuOpen){
+   menuOpen.addEventListener ('click', () => {
+   navMenu.classList.add('header__nav--visible')
+   }
+
+)}
+
+if (menuClose){
+   menuClose.addEventListener ('click', () => {
+   navMenu.classList.remove('header__nav--visible')
+   }
+
+)}
+
+
+//sroll in menu
+
+addEventListener('scroll', () => {
+ if (this.scrollY > 100)  {
+    header.classList.add ('header--fill')
+ } else {
+    header.classList.remove ('header--fill')
+ }
+}) 
+
+
