a290877466ab202a0d45568d354aa2513e5a1701
Pau-Martinez <pmartinez.2005.1109@gmail.com>
2023-11-28 23:46:32 -0500

    Se inicio formulario de productos en el localStorage

--- Assets/js/admin.js
+++ Assets/js/admin.js
@@ -11,3 +11,42 @@ if (logoutButton) {
 
    })
 }
+
+const products = JSON.parse(localStorage.getItem('PRODUCTS'))
+
+localStorage.setItem('PRODUCTS',JSON.stringify(products))
+
+const table= document.getElementById('table-content')
+
+document.addEventListener('DOMContentLoaded', () => {
+   table.innerHTML = products.map(product => (`
+   <tr>
+   <td>${product.name}</td>
+   <td>${product.description}</td>
+   <td>${product.price}</td>
+   <td>
+      <button>Editar</button>
+      <button class="button--delete">Eliminar</button>
+   </td>
+  </tr>
+   
+   `)).join('')
+})
+
+const form= document.getElementById('product-form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const product ={
+        id :products[products.length-1 ].id + 1,
+        name: event.target.elements.name.value,
+        price: event.target.elements.price.value,
+        description: event.target.elements.description.value,
+    }
+    products.push(product)
+
+    localStorage.setItem('PRODUCTS', JSON.stringify(products))
+
+    event.target.reset()
+})
--- Assets/js/index.js
+++ Assets/js/index.js
@@ -23,7 +23,7 @@ if (menuClose){
 //sroll in menu
 
 addEventListener('scroll', () => {
- if (this.scrollY > 100)  {
+ if (this.scrollY > 50)  {
     header.classList.add ('header--fill')
  } else {
     header.classList.remove ('header--fill')
